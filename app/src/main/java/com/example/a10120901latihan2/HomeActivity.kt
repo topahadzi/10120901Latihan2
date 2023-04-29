4048d2ff7fd630d5063d3a08f19058a302868541
topahadzi <topahadzi@gmail.com>
2023-04-30 04:54:25 +0700

    add identitas

--- app/src/main/java/com/example/a10120901latihan2/HomeActivity.kt
+++ app/src/main/java/com/example/a10120901latihan2/HomeActivity.kt
@@ -4,7 +4,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.material.button.MaterialButton
-
+/**
+ * Tanggal Pengerjaan : 30/04/2023
+ * NIM : 10120901
+ * Nama : Mustapha Hadzi
+ * Kelas : IF-10
+ * */
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/example/a10120901latihan2/MainActivity.kt
+++ app/src/main/java/com/example/a10120901latihan2/MainActivity.kt
@@ -5,7 +5,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import com.google.android.material.button.MaterialButton
-
+/**
+ * Tanggal Pengerjaan : 30/04/2023
+ * NIM : 10120901
+ * Nama : Mustapha Hadzi
+ * Kelas : IF-10
+ * */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
